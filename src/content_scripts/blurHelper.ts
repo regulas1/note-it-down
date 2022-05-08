e7fc4453beedd3860268ec99b2e035e8a4f38e07
Harpreet Singh <hsingh9600@gmail.com>
2022-05-08 22:57:07 +1200

    alternative implementation for blur working

--- src/content_scripts/blurHelper.ts
+++ src/content_scripts/blurHelper.ts
@@ -1,61 +1,44 @@
 export class BlurHelper {
-   private blurValue: string;
    private toBlurDivId = "toBlur";
+   private style = "";
+   private isBlurDataset = "true";
+   private isNotBlurDataset = "false";
    private toBlurSelector = `#${this.toBlurDivId}`;
 
    constructor(blurValue: string) {
-      this.blurValue = blurValue;
-      // this.initialisePage();
-   }
-
-   private initialisePage() {
-      const toBlur = document.body.appendChild(document.createElement("div"));
-      toBlur.setAttribute("id", this.toBlurDivId);
-
-      while (document.body.firstChild !== toBlur && document.body.firstChild)
-         toBlur.appendChild(document.body.firstChild);
-   }
-
-   private blurBody() {
-      // const body = document.querySelector<HTMLElement>(this.toBlurSelector);
-      // const body = document.querySelector<HTMLElement>("body > *");
-      // if (body) {
-      //    body.style.filter = this.blurValue;
-      // }
-      const style = `
+      this.style = `
          body > *:not(#notepad) {
-            filter: blur(3px);
+            filter: ${blurValue};
          }
-      `;
-
-      const stylesheet = document.createElement("style");
-      stylesheet.setAttribute("id", "blurStyle");
-      stylesheet.innerText = style;
+      `; 
+   }
+  
+   private blurBody() {
+      let stylesheet = document.getElementById("blurStyle");
+      if (!stylesheet) {
+         stylesheet = document.createElement("style");
+         stylesheet.setAttribute("id", "blurStyle");
+      }
+      stylesheet.innerText = this.style;
       document.head.appendChild(stylesheet);
+      stylesheet.dataset.isBlur = this.isBlurDataset;
    }
 
    private unBlurBody() {
-      // const body = document.querySelector<HTMLElement>("body > *");
-      // if (body) {
-      //    body.style.filter = "";
-      // }
       const stylesheet = document.getElementById("blurStyle");
       if (stylesheet) {
          stylesheet.innerText = "";
+         stylesheet.dataset.isBlur = this.isNotBlurDataset;
       }
    }
 
    private isBodyBlur() {
-      // const body = document.querySelector<HTMLElement>("body > *");
-      // if (body?.style.filter === this.blurValue) {
-      //    return true;
-      // }
-      // return false;
       const stylesheet = document.getElementById("blurStyle");
-      if (stylesheet?.innerText === "") {
-         return false;
+      console.log(stylesheet?.dataset.isBlur)
+      if (stylesheet?.dataset.isBlur === this.isBlurDataset) {
+         return true;
       }
-      return true;
+      return false;
    }
 
    public toggleBlur() {
