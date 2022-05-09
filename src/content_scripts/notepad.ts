dfec7e77958a7be28df058fc79994d16e85612d3
Harpreet Singh <hsingh9600@gmail.com>
2022-05-09 22:42:34 +1200

    add textarea

--- src/content_scripts/notepad.ts
+++ src/content_scripts/notepad.ts
@@ -7,15 +7,21 @@ export class Notepad {
    private blurHelper = new BlurHelper(8, this.notepadId);
 
    constructor() {
-      this.notepadElement = document.createElement("div");
+      this.notepadElement = document.createElement("textarea");
       this.notepadElement.setAttribute("id", this.notepadId);
       this.notepadElement.style.position = "fixed";
       this.notepadElement.style.top = "50%";
       this.notepadElement.style.left = "50%";
       this.notepadElement.style.transform = "translate(-50%, -50%)";
-      this.notepadElement.style.background = "#000";
+      this.notepadElement.style.background = "#282828";
       this.notepadElement.style.height = "50%";
       this.notepadElement.style.width = "50%";
+      this.notepadElement.style.borderRadius = "30px";
+      this.notepadElement.style.color = "#ebdbb2";
+      this.notepadElement.style.fontSize = "16px";
+      this.notepadElement.style.padding = "18px";
+      this.notepadElement.style.border = "none";
+      this.notepadElement.style.outline = "none";
    }
 
    private renderNotepad() {
