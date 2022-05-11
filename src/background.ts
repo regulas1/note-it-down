68fc8d74b6c923b3aaeadee7465228a4c0bd707f
Harpreet Singh <hsingh9600@gmail.com>
2022-05-11 12:29:29 +1200

    added normal chrome command to take note with ctrl+o

--- src/background.ts
+++ src/background.ts
@@ -1,8 +1,23 @@
 import { request } from "./types/message";
 
-chrome.action.onClicked.addListener((tab) => {
-  chrome.tabs.sendMessage<request>(
-     tab.id ? tab.id : -1,
-     { command: "blurBody" }
-  );
+chrome.commands.onCommand.addListener(async (command) => {
+   if (command === "take-note") {
+      console.log(`Command "${command}" triggered`);
+      const tab = await getCurrentTab();
+      chrome.tabs.sendMessage<request>(tab.id ? tab.id : -1, {
+         command: "blurBody",
+      });
+   }
 });
+
+chrome.action.onClicked.addListener((tab) => {
+   chrome.tabs.sendMessage<request>(tab.id ? tab.id : -1, {
+      command: "blurBody",
+   });
+});
+
+async function getCurrentTab() {
+   let queryOptions = { active: true, currentWindow: true };
+   let [tab] = await chrome.tabs.query(queryOptions);
+   return tab;
+}
