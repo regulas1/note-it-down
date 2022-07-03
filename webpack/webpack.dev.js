f67ad005e638e7012b9cc543ff80e43ce52d7358
harpreet <hsingh9600@gmail.com>
2022-07-03 17:07:29 +1200

    add webpack stuff for helping with dev

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,7 +1,26 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
+const CopyPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const srcDir = path.join(__dirname, "..", "src");
 
 module.exports = merge(common, {
     devtool: 'inline-source-map',
-    mode: 'development'
+    mode: 'development',
+    entry: {
+        notepad: path.join(srcDir, "components", "Notepad.tsx"),
+    },
+    output: {
+        path: path.join(__dirname, "../devServeDist"),
+        filename: "[name].js",
+    },
+    devServer: {
+        static: ['devServeDist']
+    },
+    plugins: [
+        new CopyPlugin({
+           patterns: [{ from: ".", to: ".", context: "devServe/" }],
+           options: {},
+        }),
+     ],
 });
